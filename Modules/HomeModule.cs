8e5a583cb1e2cbeeacf7a51e25ff642750449241
Ben Logue and Carl Egbert <ben+carl@>
2016-07-19 11:58:01 -0700

    add enroll functionality to views

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using System.Collections.Generic;
 using Registrar.Objects;
 
 namespace Registrar
@@ -8,6 +9,72 @@ namespace Registrar
     public HomeModule()
     {
       Get["/"] = _ => View["index.cshtml"];
+
+      Get["/students"] = _ =>
+      {
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
+      Get["/students/add"] = _ => View["add_student.cshtml"];
+
+      Post["/students/add"] = _ =>
+      {
+        Student newStudent = new Student(Request.Form["name"], Request.Form["date"]);
+        newStudent.Save();
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
+
+      Get["/courses"] = _ =>
+      {
+        List<Course> allCourses = Course.GetAll();
+        return View["courses.cshtml", allCourses];
+      };
+
+      Get["/courses/add"] = _ => View["add_course.cshtml"];
+
+      Post["/courses/add"] = _ =>
+      {
+        Course newCourse = new Course(Request.Form["name"], Request.Form["number"]);
+        newCourse.Save();
+        List<Course> allCourses = Course.GetAll();
+        return View["courses.cshtml", allCourses];
+      };
+
+      Get["/students/{id}"] = parameters =>
+      {
+        Student student = Student.Find(parameters.id);
+        return View["student.cshtml", student];
+      };
+
+      Get["/courses/{id}"] = parameters =>
+      {
+        Course course = Course.Find(parameters.id);
+        return View["course.cshtml", course];
+      };
+
+      Get["/students/{id}/enroll"] = parameters =>
+      {
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        Student student = Student.Find(parameters.id);
+        model.Add("student", student);
+        List<Course> allCourses = Course.GetAll();
+        model.Add("courses", allCourses);
+        return View["enroll.cshtml", model];
+      };
+
+      Patch["/students/{id}/enroll"] = parameters =>
+      {
+        Student student = Student.Find(parameters.id);
+        Course course = Course.Find(Request.Form["course"]);
+        student.AddCourse(course);
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        model.Add("student", student);
+        List<Course> allCourses = Course.GetAll();
+        model.Add("courses", allCourses);
+        return View["enroll.cshtml", model];
+      };
+
     }
   }
 
